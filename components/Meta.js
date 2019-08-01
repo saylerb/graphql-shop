9ab1a0c8241c5a22751b7a1f31fa39a74d801ad8
Brian Sayler <sayler.b@gmail.com>
2019-08-01 12:11:33 -0500

    add meta component

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" href="/static/favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="static/nprogress.css" />
+    <title>Roots Plant Shop</title>
+  </Head>
+);
+
+export default Meta;
