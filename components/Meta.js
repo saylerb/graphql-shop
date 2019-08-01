ee29a8152c11114b73f1c5fece5ffe81e3b79f2d
Brian Sayler <sayler.b@gmail.com>
2019-08-01 13:43:42 -0500

    add meta component and favicon

--- components/Meta.js
+++ components/Meta.js
@@ -3,8 +3,8 @@ import Head from "next/head";
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta charset="UTF-8" />
-    <link rel="shortcut icon" href="/static/favicon.png" type="image/x-icon" />
+    <meta charSet="UTF-8" />
+    <link rel="shortcut icon" href="/static/seedling.png" type="image/x-icon" />
     <link rel="stylesheet" type="text/css" href="static/nprogress.css" />
     <title>Roots Plant Shop</title>
   </Head>
