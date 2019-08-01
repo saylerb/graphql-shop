11b5f3a2db2d9071501436d9a120e4c522cc1f85
Brian Sayler <sayler.b@gmail.com>
2019-08-01 14:27:38 -0500

    add emotion

--- components/Page.js
+++ components/Page.js
@@ -2,12 +2,30 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
 
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+const color = "white";
+
 class Page extends Component {
   render() {
     return (
       <div>
         <Meta />
         <Header />
+        <button
+          css={css`
+            padding: 32px;
+            background-color: hotpink;
+            font-size: 24px;
+            border-radius: 4px;
+            &:hover {
+              color: ${color};
+            }
+          `}
+        >
+          Click Me!
+        </button>
         {this.props.children}
       </div>
     );
