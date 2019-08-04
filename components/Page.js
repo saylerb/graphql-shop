756302aae9f110b4be93c80e21c4f362810d84ee
Brian Sayler <sayler.b@gmail.com>
2019-08-03 19:27:52 -0500

    remove example styling, add real styling

--- components/Page.js
+++ components/Page.js
@@ -3,59 +3,25 @@ import Header from "./Header";
 import Meta from "./Meta";
 
 import styled from "@emotion/styled";
-import { css, keyframes } from "@emotion/core";
 
-const Button = styled.button`
-  padding: 32px;
-  background-color: ${props => (props.primary ? "hotpink" : "turquoise")};
-  font-size: 24px;
-  border-radius: 4px;
-  &:hover {
-    color: white;
-  }
+const StyledPage = styled.div`
+  background: white;
+  color: black;
 `;
 
-const spin = keyframes`
-  to {
-    transform: rotate(360deg)
-  }
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 const Page = props => {
-  const [spinSpeed, setSpinSpeed] = useState(2);
-
   return (
-    <div>
+    <StyledPage>
       <Meta />
       <Header />
-      <span>{spinSpeed}s</span>
-      <Button primary onClick={() => setSpinSpeed(spinSpeed + 0.5)}>
-        Slowers
-      </Button>
-      <Button
-        onClick={() => {
-          if (spinSpeed - 0.5 > 0) {
-            setSpinSpeed(spinSpeed - 0.5);
-          }
-        }}
-      >
-        Fasters
-      </Button>
-      <div
-        css={css`
-          font-size: 100px;
-          display: inline-block;
-          animation: ${spinSpeed}s ${spin} linear infinite;
-
-          &:hover {
-            animation: ${spinSpeed}s ${spin} linear infinite reverse;
-          }
-        `}
-      >
-        🌱
-      </div>
-      {props.children}
-    </div>
+      <Inner>{props.children}</Inner>
+    </StyledPage>
   );
 };
 
