318cd2330dab75923d868f2bc08b641f221a35ec
Brian Sayler <sayler.b@gmail.com>
2019-08-05 10:47:48 -0500

    add emotion-theming

--- components/Page.js
+++ components/Page.js
@@ -3,6 +3,17 @@ import Header from "./Header";
 import Meta from "./Meta";
 
 import styled from "@emotion/styled";
+import { ThemeProvider } from "emotion-theming";
+
+const theme = {
+  red: "#D66853",
+  maroon: "#7D4E57",
+  grey: "#64156",
+  black: "#11151C",
+  green: "#04471C",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
 
 const StyledPage = styled.div`
   background: white;
@@ -10,18 +21,22 @@ const StyledPage = styled.div`
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+  background-color: ${props => props.theme.green};
+  color: white;
 `;
 
 const Page = props => {
   return (
-    <StyledPage>
-      <Meta />
-      <Header />
-      <Inner>{props.children}</Inner>
-    </StyledPage>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <Inner>{props.children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   );
 };
 
