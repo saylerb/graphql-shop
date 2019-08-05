2647cca743513c6c1304c383abe9606739a5577c
Brian Sayler <sayler.b@gmail.com>
2019-08-05 17:32:47 -0500

    add styled logo

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,33 @@
 import Nav from "./Nav";
+import Link from "next/link";
+import styled from "@emotion/styled";
+
+const Logo = styled.h1`
+  font-size: 4em;
+  margin-left: 2rem;
+  /* position: relative; */
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.green};
+    color: white;
+    text-transform: lowercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
 
 const Header = () => (
   <div>
     <div className="bar">
-      <a href="">Roots Plant Shop</a>
+      <Logo>
+        <Link href="/">
+          <a>Roots</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
 
