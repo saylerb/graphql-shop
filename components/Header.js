f91ff3dba44092399775efa19f0b009625fcb184
Brian Sayler <sayler.b@gmail.com>
2019-08-07 09:32:38 -0500

    add nprogress loading bar

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,18 @@
 import Nav from "./Nav";
 import Link from "next/link";
 import styled from "@emotion/styled";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4em;
